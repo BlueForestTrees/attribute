1c385efb4893f6f772e1988282006d404d2d2802
slim <smedini@gmail.com>
2018-09-20 17:59:24 +0200

    welcome metamodule!

--- src/rest/entry/deleteAttributeEntryRest.js
+++ src/rest/entry/deleteAttributeEntryRest.js
@@ -1,12 +1,15 @@
 import {run} from 'express-blueforest'
 import {Router} from "express-blueforest"
 import {cols} from "../../collections"
+import {col} from "mongo-registry"
 import ENV from "./../../env"
 
 const router = Router()
 
 module.exports = router
 
-router.delete(`/api/${ENV.NAME}Entry`, run(col(cols.ATTRIBUTE_ENTRY)().deleteMany))
+const attributes = ()=>col(cols.ATTRIBUTE_ENTRY)
+
+router.delete(`/api/${ENV.NAME}Entry`, run(attributes().deleteMany))
 
 
