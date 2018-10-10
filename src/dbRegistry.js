bfd1b01d19b27ccc9098455029aa372911e3f115
slim <smedini@gmail.com>
2018-10-10 13:19:31 +0200

    attributes index

---
+++ src/dbRegistry.js
@@ -0,0 +1,15 @@
+import {col} from "mongo-registry"
+import {cols} from "./collections"
+
+export const registry = [
+    {
+        version: "1.0.4",
+        log: "externId idx",
+        script: () => col(cols.ATTRIBUTE_ENTRY).createIndex({"externId": 1}, {unique: true, partialFilterExpression: {externId: {$exists: true}}})
+    },
+    {
+        version: "1.0.5",
+        log: "facet (trunkId,facetId) idx",
+        script: () => col(cols.ATTRIBUTE).createIndex({"trunkId": 1, "facetId": 1})
+    }
+]
