601cdd0089536f7054f19e045fd0e520effb5fbd
slim <smedini@gmail.com>
2019-04-08 18:03:54 +0200

    correct registry

--- src/dbRegistry.js
+++ src/dbRegistry.js
@@ -9,13 +9,8 @@ export default [
         script: () => col(cols.ATTRIBUTE_ENTRY).createIndex({"externId": 1}, {unique: true, partialFilterExpression: {externId: {$exists: true}}})
     },
     {
-        version: "1.0.5",
-        log: "facet (trunkId,facetId) idx",
-        script: () => col(cols.ATTRIBUTE).createIndex({"trunkId": 1, "facetId": 1})
-    },
-    {
         version: "1.1.8",
         log: `facet (trunkId,${ENV.NAME}Id) idx`,
-        script: () => col(cols.ATTRIBUTE).createIndex({[ENV.NAME + "Id"]: 1, "facetId": 1})
+        script: () => col(cols.ATTRIBUTE).createIndex({"trunkId": 1, [ENV.NAME + "Id"]: 1})
     }
 ]
