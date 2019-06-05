2f8fa017f8728f7ae2c22037c1f1ceffd9b709ec
slim <smedini@gmail.com>
2019-06-05 14:19:29 +0200

    1.2.1 - collecion name become 'ENV.NAME'

--- src/collections.js
+++ src/collections.js
@@ -1,7 +1,7 @@
 import ENV from "./env"
 
 export const cols = {
-    ATTRIBUTE: `${ENV.NAME}Collection`,
+    ATTRIBUTE: `${ENV.NAME}`,
     ATTRIBUTE_ENTRY: `${ENV.NAME}EntryCollection`,
     ATTRIBUTE_TANK: `${ENV.NAME}Tank`
 }
